3df8923d85262f27229e71cf3e9d67e3501d8f2d
pczyzewski <p.czyzewski5@gmail.com>
2023-03-07 19:08:15 +0100

    fixed make build

--- sql/28-02-2023_create_answers_table.sql
+++ sql/28-02-2023_create_answers_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE answers
     id             VARCHAR(36) NOT NULL,
     question_id    VARCHAR(36) NOT NULL,
     answer         TEXT NOT NULL,
-    is_correct       BOOLEAN NOT NULL,
+    is_correct     BOOLEAN NOT NULL,
     created_at     DATETIME NOT NULL,
     UNIQUE (id)
 ) DEFAULT CHARACTER SET UTF8
