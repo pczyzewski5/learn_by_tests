479433d4f8a63462c01e6f32be5182fbf34277e9
pczyzewski <p.czyzewski5@gmail.com>
2023-03-08 00:34:00 +0100

    splitted out main.js

---
+++ public/js/quill.js
@@ -0,0 +1,21 @@
+let $container = $('#quill-editor-container');
+let $dataContainer = $('#quill-data-container');
+
+if ($container.length != 0 && $dataContainer.length != 0) {
+    $quill = new Quill($container.get(0), {
+        theme: 'snow'
+    });
+
+    $quill.on('text-change', function($delta, $oldDelta, $source) {
+        if ($source == 'api') {
+            console.log("An API call triggered this change.");
+        }
+        if ($source == 'user') {
+            $dataContainer.val(JSON.stringify($quill.getContents()));
+        }
+    });
+
+    if ($dataContainer.val().length != 0) {
+        $quill.setContents(JSON.parse($quillData.val()));
+    }
+}
---
+++ public/js/select_valid_answer.js
@@ -0,0 +1,13 @@
+let $answer = $('.answer');
+let $attr = $answer.attr('answer-id');
+
+if (typeof $attr !== 'undefined' && $attr !== false) {
+    $answer.click(function () {
+        $(this).removeClass('selected-answer');
+        $(this).addClass('selected-answer');
+        $('#correct_answer_form_is_correct_answer_field').val(
+            $(this).attr('answer-id')
+        );
+        $('#correct_answer_form_save').removeAttr('disabled');
+    });
+}
