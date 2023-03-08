cb90423f7740c792bb616ef49af31a8986f007e8
pczyzewski <p.czyzewski5@gmail.com>
2023-03-09 00:18:53 +0100

    fixed js errors

--- public/js/select_valid_answer.js
+++ public/js/select_valid_answer.js
@@ -1,13 +1,23 @@
-let $answer = $('.answer');
-let $attr = $answer.attr('answer-id');
+class SelectValidAnswer {
+    constructor() {
+        this.execute()
+    }
 
-if (typeof $attr !== 'undefined' && $attr !== false) {
-    $answer.click(function () {
-        $('.answer').removeClass('selected-answer');
-        $(this).addClass('selected-answer');
-        $('#correct_answer_form_is_correct_answer_field').val(
-            $(this).attr('answer-id')
-        );
-        $('#correct_answer_form_save').removeAttr('disabled');
-    });
+    execute() {
+        let $answer = $('.answer');
+        let $attr = $answer.attr('answer-id');
+
+        if (typeof $attr !== 'undefined' && $attr !== false) {
+            $answer.click(function () {
+                $('.answer').removeClass('selected-answer');
+                $(this).addClass('selected-answer');
+                $('#correct_answer_form_is_correct_answer_field').val(
+                    $(this).attr('answer-id')
+                );
+                $('#correct_answer_form_save').removeAttr('disabled');
+            });
+        }
+    }
 }
+
+new SelectValidAnswer();
