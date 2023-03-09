f570ffa737e31e4391da9cc165a76aa53d371b35
pczyzewski <p.czyzewski5@gmail.com>
2023-03-09 21:09:19 +0100

    added symfony debug packages

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['all' => true],
 ];
