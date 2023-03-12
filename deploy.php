09af39820c3099afad3b508f17876867926035e2
pczyzewski <p.czyzewski5@gmail.com>
2023-03-12 01:53:16 +0100

    upgraded deploy.php

--- deploy.php
+++ deploy.php
@@ -4,6 +4,8 @@ namespace Deployer;
 
 require 'contrib/rsync.php';
 
+const APP_DIR = '/home/southpaw/domains/learnbytests.pl';
+
 // Config
 set('rsync', [
     'exclude'      => [
@@ -37,21 +39,43 @@ set('rsync', [
     'timeout'      => 60,
 ]);
 set('rsync_src', '/data/application');
-set('rsync_dest', '/home/southpaw/domains/learnbytests.pl');
-set('deploy_path', '/home/southpaw/domains/learnbytests.pl');
-
-// Tasks
-task('install vendors', function() {
-    run('cd /home/southpaw/domains/learnbytests.pl && composer install');
-});
+set('rsync_dest', APP_DIR . '/new_release');
 
 // Hosts
 host('s148.cyber-folks.pl')
     ->set('remote_user', 'southpaw')
     ->set('port', 222);
 
+// Tasks
+task('create previous_release dir', function() {
+    run('rm -rf ' . APP_DIR . '/previous_release &&
+    mkdir -p ' . APP_DIR . '/previous_release');
+});
+task('copy current_release to previous_release', function() {
+    run('cp -r ' . APP_DIR . '/current_release/* ' . APP_DIR . '/previous_release/');
+});
+task('create current_release dir', function() {
+    run('rm -rf ' . APP_DIR . '/current_release &&
+    mkdir -p ' . APP_DIR . '/current_release');
+});
+task('copy new_release to current_release', function() {
+    run('cp -r ' . APP_DIR . '/new_release/* ' . APP_DIR . '/current_release/');
+});
+task('copy prod env file', function() {
+    run('cp ' . APP_DIR . '/.env.prod ' . APP_DIR . '/current_release/');
+});
+task('install vendors', function() {
+    run('cd ' . APP_DIR . '/current_release && composer install');
+});
+
+
 // Hooks
 task('deploy', [
+    'create previous_release dir',
+    'copy current_release to previous_release',
     'rsync',
-    'install vendors'
+    'create current_release dir',
+    'copy new_release to current_release',
+    'copy prod env file',
+    'install vendors',
 ])->desc('Deploy your project');
