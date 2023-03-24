83c84c295165ef38d09fab9710f448ff3fde481d
pczyzewski <p.czyzewski5@gmail.com>
2023-03-24 19:09:04 +0100

    added new routes for user

--- src/App/Controller/LoginController.php
+++ src/App/Controller/LoginController.php
@@ -9,9 +9,21 @@ class LoginController extends BaseController
 {
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('login/login.html.twig', [
-            'last_username' => $authenticationUtils->getLastUsername(),
-            'error' => $authenticationUtils->getLastAuthenticationError()
-        ]);
+        $user = $this->getUser();
+
+        if (null === $user) {
+            return $this->render('login/login.html.twig', [
+                'last_username' => $authenticationUtils->getLastUsername(),
+                'error' => $authenticationUtils->getLastAuthenticationError()
+            ]);
+        }
+
+        if ($this->isGranted('ROLE_SUPER_ADMIN')
+            || $this->isGranted('ROLE_ADMIN')
+        ) {
+            return $this->redirectToRoute('category_list');
+        }
+
+        return $this->redirectToRoute('user_category_list');
     }
 }
--- src/App/Controller/UserController.php
+++ src/App/Controller/UserController.php
@@ -6,6 +6,11 @@ namespace App\Controller;
 
 use App\CommandBus\CommandBus;
 use App\QueryBus\QueryBus;
+use LearnByTests\Domain\Category\CategoryEnum;
+use LearnByTests\Domain\Query\FindQuestions;
+use LearnByTests\Domain\Query\GetCategories;
+use LearnByTests\Domain\Query\GetSubCategories;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends BaseController
@@ -19,10 +24,38 @@ class UserController extends BaseController
         $this->commandBus = $commandBus;
     }
 
-    public function index(): Response
+    public function categoryList(): Response
     {
-        return $this->renderForm('index/index.html.twig', [
-            'php_info' => 'yolo'
+        return $this->renderForm('user/category_list.html.twig', [
+            'categories' => $this->queryBus->handle(
+                new GetCategories()
+            )
+        ]);
+    }
+
+    public function testList(Request $request): Response
+    {
+        $subcategory = $request->get('subcategory');
+        $category = CategoryEnum::fromLowerKey(
+            $request->get('category')
+        );
+        $subcategories = $this->queryBus->handle(
+            new GetSubCategories($category)
+        );
+        $questions = $this->queryBus->handle(
+            new FindQuestions(
+                $category,
+                null === $subcategory
+                    ? null
+                    : CategoryEnum::fromLowerKey($subcategory)
+            )
+        );
+
+        return $this->renderForm('admin/question_list.html.twig', [
+            'category' => $category,
+            'subcategories' => $subcategories,
+            'active_subcategory' => $subcategory,
+            'questions' => $questions,
         ]);
     }
 }
