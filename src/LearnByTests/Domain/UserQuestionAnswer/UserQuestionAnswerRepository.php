d3b9f22e059944cea6d73803e1e13fff54eefa6e
pczyzewski <p.czyzewski5@gmail.com>
2023-03-24 18:17:06 +0100

    fixed UserQuestionAnswerRepository.php interface

--- src/LearnByTests/Domain/UserQuestionAnswer/UserQuestionAnswerRepository.php
+++ src/LearnByTests/Domain/UserQuestionAnswer/UserQuestionAnswerRepository.php
@@ -5,16 +5,12 @@ declare(strict_types=1);
 namespace LearnByTests\Domain\UserQuestionAnswer;
 
 use LearnByTests\Domain\UserQuestionAnswer\Exception\UserQuestionAnswerNotFoundException;
+use LearnByTests\Domain\UserQuestionAnswer\UserQuestionAnswer;
 
 interface UserQuestionAnswerRepository
 {
     /**
      * @throws UserQuestionAnswerNotFoundException
      */
-    public function getOneById(string $id): UserQuestionAnswer;
-
-    /**
-     * @return UserQuestionAnswer[]
-     */
-    public function findAll(): array;
+    public function findOne(string $userId, string $questionId): ?UserQuestionAnswer;
 }
