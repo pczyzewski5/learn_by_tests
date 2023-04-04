5995456b4457f14cebdcf3e58e1ae799e1856a7c
pczyzewski <p.czyzewski5@gmail.com>
2023-04-04 17:45:46 +0200

    fixed UnderDevHandler.php

--- src/LearnByTests/Domain/Query/UnderDevHandler.php
+++ src/LearnByTests/Domain/Query/UnderDevHandler.php
@@ -19,24 +19,21 @@ class UnderDevHandler
 
     public function __invoke(UnderDev $query): array
     {
-        $sql = 'SELECT q.id, q.question, q.category, q.subcategory, a.is_correct FROM questions q
-                    LEFT JOIN user_question_answers uqa ON q.id = uqa.question_id
+        $sql = 'SELECT q.id, q.question, q.category, q.subcategory, a.is_correct, (usq.question_id IS NOT NULL) as is_skipped FROM questions q
+                    LEFT JOIN user_question_answers uqa ON q.id = uqa.question_id AND (uqa.user_id = :userId OR uqa.user_id IS NULL)
                     LEFT JOIN answers a ON a.id = uqa.answer_id
-                WHERE (uqa.user_id = :userId OR uqa.user_id IS NULL)
-                    AND q.category = :category
+                    LEFT JOIN user_skipped_questions usq ON usq.question_id = q.id AND (usq.user_id = :userId OR usq.user_id IS NULL)
+                WHERE q.category = :category
                     AND q.subcategory LIKE :subcategory;';
 
-        $subcategory = '%';
-        if (null !== $query->getSubcategory()) {
-            $subcategory = $query->getSubcategory()->getLowerKey();
-        }
+        $subcategory = $query->getSubcategory();
 
         $stmt = $this->entityManager->getConnection()->executeQuery(
             $sql,
             [
                 'userId' => $query->getUserId(),
                 'category' => $query->getCategory()->getLowerKey(),
-                'subcategory' => $subcategory,
+                'subcategory' => null === $subcategory ? '%' : $subcategory->getLowerKey(),
             ],
             [
                 'userId' => Types::STRING,
@@ -51,10 +48,9 @@ class UnderDevHandler
             if (null !== $item['is_correct']) {
                 $item['is_correct'] = (int)$item['is_correct'];
             }
-            //            if (null !== $item['is_skipped']) {
-            //                $item['is_skipped'] = (int)$item['is_skipped'];
-            //            }
-            $item['is_skipped'] = 0;
+            if (null !== $item['is_skipped']) {
+                $item['is_skipped'] = (int)$item['is_skipped'];
+            }
 
             $result[] = $item;
         }
